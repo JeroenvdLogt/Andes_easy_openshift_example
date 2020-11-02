7b6f6a79a9ecad005f2db025cb84a34653410b35
Jeroen van de Logt <jeroen.vande.logt@qirion.nl>
2020-11-02 10:33:45 +0100

    debugged

--- easy_calc.R
+++ easy_calc.R
@@ -1,5 +1,5 @@
 # Function with easy calculation
 easy_calc <- function(year) {
   result <- 100 + year
-  saveRDS(object = result, file = paste0('easy_calc_', year, '.rds'))
+  saveRDS(object = result, file = easypaste0('easy_calc_', year, '.rds'))
 }
--- round_up_results.R
+++ round_up_results.R
@@ -1,6 +1,9 @@
 # Round  up all results
 round_up_results <- function(years) {
+  res <- list()
   for(year in years){
-    res <- readRDS(file = paste0('easy_calc_', year, '.rds'))
+    res[[year]] <- readRDS(file = paste0('easy_calc_', year, '.rds'))
   }
+  round_up <- sum(unlist(res))
+  saveRDS(object = round_up, 'round_up.rds')
 }
