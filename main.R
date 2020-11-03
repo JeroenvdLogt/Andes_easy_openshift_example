267637172a04640bd048a068108b52f7c7821b00
JeroenvdLogt <jeroen.vande.logt@qirion.nl>
2020-11-03 09:57:15 +0100

    correct filepahts for sourcing in main.R

--- main.R
+++ main.R
@@ -10,8 +10,8 @@ rlib <- paste0('/usr/app/Rlibs')
 
 # main call
 .libPaths(rlib)
-source(easy_calc.R)
-source(round_up_results.R)
+source('/usr/app/easy_calc.R')
+source('/usr/app/round_up_results.R')
 for(year in 2020:2059){easy_calc(year)}
 round_up_results(2020:2059)
 print(readRDS(round_up.rds)
