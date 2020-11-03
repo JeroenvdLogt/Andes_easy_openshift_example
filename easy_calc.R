2b6c60f9e33a32c365b99b3153fc31a4c6ca2e12
JeroenvdLogt <jeroen.vande.logt@qirion.nl>
2020-11-03 12:03:23 +0100

    ls -lR right after saveRDS

--- easy_calc.R
+++ easy_calc.R
@@ -3,4 +3,5 @@ easy_calc <- function(year) {
   result <- 100 + year
   eval(parse(text=paste0("system('echo ",year,"   ')")))
   saveRDS(object = result, file = paste0('easy_calc_', year, '.rds'))
+  eval(parse(text="system('ls -lR ./')"))
 }
