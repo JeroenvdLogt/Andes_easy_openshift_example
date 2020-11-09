145cc69e6219f03a1644680d24b9d9c8dfd81a77
JeroenvdLogt <jeroen.vande.logt@qirion.nl>
2020-11-09 10:55:21 +0100

    /data-data

--- easy_calc.R
+++ easy_calc.R
@@ -2,6 +2,6 @@
 easy_calc <- function(year) {
   result <- 100 + year
   eval(parse(text=paste0("system('echo ",year,"   ')")))
-  saveRDS(object = result, file = paste0('data-data/easy_calc_', year, '.rds'))
-  #eval(parse(text="system('ls -lR ./data-data/')"))
+  saveRDS(object = result, file = paste0('/data-data/easy_calc_', year, '.rds'))
+  #eval(parse(text="system('ls -lR /data-data/')"))
 }
